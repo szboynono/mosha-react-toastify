ccd276a3ced10ee62e45771de347d70a855f8849
szboynono <szboylbd@hotmail.com>
2021-06-21 23:22:07 -0400

    feat: finish setup

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+export type Test = 'ni' | 'hao'
+
+export const yo = (arg: Test) => {
+  console.log(arg)
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, '/src/main.ts'),
+      name: 'mosha-react-toastify'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
 })
